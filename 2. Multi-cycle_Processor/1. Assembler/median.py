d50cf4838caf6911ba380fe0875cfd74ed1a85ef
willzma <willzma15@gmail.com>
2017-03-09 04:09:44 -0500

    Bunch of resets, cleanup

--- 2. Multi-cycle_Processor/1. Assembler/median.py
+++ 2. Multi-cycle_Processor/1. Assembler/median.py
@@ -4,4 +4,4 @@ array = []
 start = 13
 for i in range(0, 4095):
     array.append(start + (i * 11))
-print("{0:0X}".format(int(statistics.median(array))))
+print("0x" + "{0:0X}".format(int(statistics.median(array))))
